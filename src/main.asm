c00d0b8911378656a8f482af60fa7611cd92a8aa
Remi laptop <remi.bismuth@gmail.com>
2015-02-28 14:41:50 +0100

    Initialization

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,7 @@
 INCLUDE "gbhw.inc"
 INCLUDE "memory.inc"
 INCLUDE "input.inc"
+INCLUDE "display.inc"
 
 
 ; Entry point
@@ -13,7 +14,36 @@ main:
 
 initialize:
 	di
-    ld	sp      , $ffff
+    ld	sp,	$ffff
+
+	; Init Palettes
+	ld	a,			%11100100
+	ld	[rBGP],		a
+	ld	[rOBP0],	a
+	ld	a,			%00011011
+	ld	[rOBP1],	a
+
+	; Init scroll
+	ld	a,		0
+	ld	[rSCX],	a
+	ld	[rSCY],	a
+
+	call	disable_lcd
+
+	; Empty background
+	ld		de,		_SCRN0
+	ld		bc,		32*32
+	ld		l,		0
+	call	mem_fill
+
+	; Empty sprites
+	ld		de,		_OAMRAM
+	ld		bc,		40*4
+	ld		l,		0
+	call	mem_fill
+
+	; Set LCD
+	ld	a,	LCDCF_ON|LCDCF_BG8000|LCDCF_BG9800|LCDCF_OBJ8|LCDCF_BGON|LCDCF_WINOFF|LCDCF_OBJOFF
 
 loop:
 	nop
