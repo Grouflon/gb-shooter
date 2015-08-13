f588a547bf96dc2f5849b34d36104d13a7fce0a6
Remi laptop <remi@uselessworks.com>
2015-08-13 06:31:26 +0200

    Windows build system ok

--- build.bat
+++ build.bat
@@ -8,19 +8,15 @@ set MAIN_FILE=main.asm
 set OBJ_FILE=main.o
 set LINKFILE=linkfile
 
-
+@echo compiling...
 mkdir %OBJ_DIR% > NUL 2> NUL
 .\tools\rgbasm.exe -i%SRC_DIR% -o%OBJ_DIR%%OBJ_FILE% %SRC_DIR%%MAIN_FILE%> NUL
 
-@echo Writing linkfile...
-@echo [Objects]> %OBJ_DIR%%LINKFILE%
-@echo %OBJ_DIR%%OBJ_FILE%>> %OBJ_DIR%%LINKFILE%
-@echo [Output]>> %OBJ_DIR%%LINKFILE%
-@echo %BIN_DIR%%TARGET%>> %OBJ_DIR%%LINKFILE%
-
+@echo linking...
 mkdir %BIN_DIR% > NUL 2> NUL
-.\tools\rgblink.exe %OBJ_DIR%%LINKFILE%
+.\tools\rgblink.exe -o %BIN_DIR%%TARGET% %OBJ_DIR%%OBJ_FILE%
 
+@echo fixing...
 .\tools\rgbfix.exe -p0 -v %BIN_DIR%%TARGET%
 
 if %ERRORLEVEL% NEQ 0 PAUSE
